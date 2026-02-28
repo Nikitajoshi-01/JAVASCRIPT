ad0d73f03812547812ff0b766f1c71339f3c7362
Nikita Joshi <joshinikita873@gmail.com>
2026-02-28 19:09:45 +0530

    newlu updated vrojjj

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+// let score="33ab";
+let score="null";
+console.log(typeof score);
+console.log(typeof (score));
+let valinnum=Number(score); // has converted the string score to number 
+console.log(typeof valinnum);
+// console.log(valinnum+3);  // tha datat type is still n sometines the string might not be convertible uber but
+console.log(valinnum);  // tha datat type is still n sometines the string might not be convertible uber but
+// 1= true, 0=false
+// ""=false;
+// "hotesh "=true
+
+let num=13;
+let stnum=String(num);
+console.log(typeof stnum);
+console.log(stnum+1);       //131
