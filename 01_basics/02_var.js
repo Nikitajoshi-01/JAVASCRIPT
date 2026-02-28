72d918ef701546f7aaca5ec19313fbde22df0426
Nikita Joshi <joshinikita873@gmail.com>
2026-02-28 17:50:01 +0530

    added 02_var.js

---
+++ 01_basics/02_var.js
@@ -0,0 +1,14 @@
+const accId=1222
+let accEmail
+var accPass="1234"
+accCity="jaipur"
+// error cant assign to const var
+// accId=2
+//dont use var bc block and functuon scope
+
+
+console.log(accId);
+// accEmail="newbpoi"
+accCity="delhi"
+accPass="2344"
+console.table([accId,accEmail,accCity,accPass]);
