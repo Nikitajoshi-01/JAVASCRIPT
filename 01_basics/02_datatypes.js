ac807295bdf8e017cfb9c1e3f0f486e2e97f01c1
Nikita Joshi <joshinikita873@gmail.com>
2026-02-28 18:41:03 +0530

    this is updated datattye

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" ;
+//treat all js code as newer version of js
+
+// alert(3+3);
+let name="nikki";
+let age =18;
+let boli=false;
+//number=2 to power 53
+//bigint=
+// string =""
+// boolean=true ' false;
+// null=empty value -standalone 
+// undefined=variable not assigned 
+//symbol=unique value 
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof boli);
+console.log(typeof null); //object
+console.log(typeof undefined);
