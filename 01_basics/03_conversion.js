6b4986b14379a61bbb7a1180ac85dd51e69e44f1
Nikita Joshi <joshinikita873@gmail.com>
2026-03-01 16:54:17 +0530

    updated this .

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -14,3 +14,25 @@ let num=13;
 let stnum=String(num);
 console.log(typeof stnum);
 console.log(stnum+1);       //131
+//operators:
+let value =3;
+let negvalue =-value;
+console.log(negvalue);
+let str1="hello";
+let str2="world";
+str3=str1+str2;
+console.log(str3);
+console.log("1"+3);   //13
+console.log(1+"2");   //12
+console.log("1"+"2");  //12
+console.log("1"+2+3) // 123
+console.log(1+"2"+3) // 123
+console.log(1+2+"3") // 33
+console.log(true);
+console.log(+true);  //1
+console.log(true+2) //3
+console.log(+"");  //0
+num1=num2=num3=2+3;
+console.log(num1,num2,num3);
+num1++;
+console.log(num1);
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+console.log(2>1);
+console.log(2==1);
+console.log(2===2.0);
+console.log(2=="2");
+console.log(2==="2");
+console.log("2">1);
+console.log("02">1);
+console.log(null==undefined);
+console.log(null===undefined);
+console.log(null>0);
+console.log(null==0); // js converted null to 0 and then compared
+console.log(null>=0);
+console.log(undefined>0)
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//comparision vs equality check 
+
+
+//strict equality check === checks for value and data type both
+//loose equality check == checks for value only and does type coercion if data types are different --- it converts the 2 diff data tyeps
