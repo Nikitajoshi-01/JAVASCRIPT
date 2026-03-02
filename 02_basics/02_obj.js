7e3de2bea2da5bb596304656ec581b29d0de9eea
Nikita Joshi <joshinikita873@gmail.com>
2026-03-02 12:18:51 +0530

    json wagera

--- 02_basics/02_obj.js
+++ 02_basics/02_obj.js
@@ -25,3 +25,29 @@ obj1.greet=function(){
     console.log(`${this.name}`);
 }
 console.log(obj1.greet()); // here the function has not been executed but uts reference is stored in the object
+console.log(obj1.hasOwnProperty("name")); // this method checks if the object has the property or not
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const course={
+    coursename:"javascript",
+    price: 233,
+    instructor: "Nikki",
+}
+console.log(course.coursename); // this is the dot notation to access the property of the object
+console.log(course["price"]); // this is the bracket notation to access the property of the object
+const {cinst}=course; // this is the destructuring assignment to access the property of the object
+console.log(cinst); // this will give us undefined because we have not defined the variable cinst in the object
+const {instructor:inst}=course; // this is the destructuring assignment to access the property of the object
+console.log("hie",inst); // this will give us the value of the instructor property of the object
---
+++ 02_basics/03_objapi.js
@@ -0,0 +1,19 @@
+// //api and json 
+
+///// this is a json format !
+//json is like an obj but it is in string format
+// {
+//     "name":"nikita ",
+//     "class":"CSE",
+//     "age":18,
+// }
+
+
+
+
+// // we can also get array of objects as api 
+// [
+//     {},
+//     {},
+//     {}
+// ]
