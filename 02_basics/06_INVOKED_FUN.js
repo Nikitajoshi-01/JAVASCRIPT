a391acce35946acaed9f26f6a7d903792df26b43
Nikita Joshi <joshinikita873@gmail.com>
2026-03-02 22:11:43 +0530

    niki@desktop

--- 02_basics/06_INVOKED_FUN.js
+++ 02_basics/06_INVOKED_FUN.js
@@ -3,3 +3,7 @@
     console.log(`welcome to code with nikki`)
 })();
 // iife is used to create a new scope and to avoid polluting the global scope with variables and functions and it is also used to execute a function immediately after its definition without having to call it separately.
+
+((name)=>{
+    console.log(`welcome to code with ${name}`);
+})("nikjoshi");
